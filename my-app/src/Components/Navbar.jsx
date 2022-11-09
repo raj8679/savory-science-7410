0b051bfe0c8e2454bade33b44cd22296b8402ab9
raj8679 <devraj8679@gmail.com>
2022-11-09 19:57:10 +0530

    added

---
+++ my-app/src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import {Box,Text} from "@chakra-ui/react";
+
+function Navbar(){
+    return (
+        <>
+<Box bg="blue.100" p="5px">
+<Text textAlign="center">Join our webinar to see how new Airtable features can transform your work <a href="">Register -></a></Text>
+</Box>
+
+        </>
+    )
+}
+
+export default Navbar;
