74250328e99808ff2f0ab8c1d78f42d9bef8a5e0
raj8679 <devraj8679@gmail.com>
2022-11-11 19:47:25 +0530

    m

--- my-app/src/Components/Modal_Pro.jsx
+++ my-app/src/Components/Modal_Pro.jsx
@@ -11,6 +11,8 @@ import {
     Stack,
     Radio,
     Button,
+    Box,
+    Text,
 
   } from '@chakra-ui/react'
 
@@ -39,8 +41,35 @@ import {
           <DrawerContent>
             <DrawerHeader borderBottomWidth='1px'></DrawerHeader>
             <DrawerBody>
-             
+             <Box >
+             <Text fontSize="xl" fontWeight="bold">Overview</Text>
+              <Text fontSize="lg">Welcome to Airtable! Here's the basics</Text>
+              </Box>
+              <Box marginTop="20px">
+             <Text fontSize="xl" fontWeight="bold">Features</Text>
+              <Text fontSize="lg">Discover automation,views,reporting,and more</Text>
+              </Box>
+              <Box marginTop="20px">
+             <Text fontSize="xl" fontWeight="bold">Integrations</Text>
+              <Text fontSize="lg">Bring your favourite tool into Airtable</Text>
+              </Box>
+              <Box marginTop="20px">
+             <Text fontSize="xl" fontWeight="bold">Enterprcie Overview</Text>
+              <Text fontSize="lg">See how Airtable scales for large and complex organisation</Text>
+              </Box>
+              <Box marginTop="20px" fontSize="xl" fontWeight="bold">Market Place</Box>
+              <Box marginTop="20px" fontSize="xl" fontWeight="bold">Download Apps</Box>
+              <Box marginTop="20px" fontSize="xl" fontWeight="bold">What's New</Box>
             </DrawerBody>
+
+            <DrawerFooter>
+              <Box bg="#e9f6fb" w="100%" h="200px">
+                <Text marginTop="30px" marginLeft="20px" fontSize="xl" fontWeight="bold">Pricing</Text>
+                <Text marginTop="20px" marginLeft="20px">From small business to global enterprise, there’s an Airtable plan that’s just right for you.</Text>
+                <Text  marginLeft="20px" color="navy">Explore Pricing</Text>
+
+              </Box>
+            </DrawerFooter>
           </DrawerContent>
         </Drawer>
       </>
